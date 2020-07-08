8eb5cee2e9e9188b12ac6d866b399b8d8957cc93
psjishnu <psjishnu13@gmail.com>
2020-07-08 09:23:37 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
   app.enableCors({ origin: '*' });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   const port = process.env.PORT || 8000;
-  await app.listen(port);}
+  await app.listen(port);
+}
 bootstrap();
