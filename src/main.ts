edb80225de77d01dce35cb6a939182faa1cfcc04
psjishnu <psjishnu13@gmail.com>
2020-07-08 09:10:58 +0530

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import * as config from "config"
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const serverConfig = config.get("server");
+
   app.enableCors({ origin: '*' });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-
-  await app.listen(8000);
-}
+  const port = parseInt(process.env.PORT) || 8000;
+  await app.listen(port);}
 bootstrap();
